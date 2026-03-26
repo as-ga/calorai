ed28ac933ba6d38aaf8452116e8545c84a2f3ffd
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2026-03-26 10:58:08 +0530

    feat: integrate Tailwind CSS

--- calorai-dasboard/src/App.tsx
+++ calorai-dasboard/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div>
-      <h1>CalorAI Dashboard</h1>
+    <div className="bg-white h-screen w-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">CalorAI Dashboard</h1>
     </div>
   );
 }
